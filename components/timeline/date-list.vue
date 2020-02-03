1a4b8806dba73ef1f2640dc20eeeab0ab761b3f5
Félix Bouveret <bouveret.felix@gmail.com>
2020-02-03 14:51:25 +0100

    Decades titles animations

--- components/timeline/date-list.vue
+++ components/timeline/date-list.vue
@@ -8,8 +8,12 @@
 
     <template v-else>
       <section v-for="decade in decadeList" :key="decade" class="date-list-container decades">
+        <h2 class="decade-title" :class="{decade : decade }">
+          <div class="inner">
+            <span>{{ decade }}</span>
+          </div>
+        </h2>
         <div class="inner">
-          <h2>{{ decade }}</h2>
           <transition-group name="date-in-out" tag="div" class="date-list">
             <date v-for="(date) in allDates"
                   :key="date.slug"
@@ -44,7 +48,41 @@ export default {
     isSearching () {
       return this.$store.state.isSearching
     }
-  }
+  },
+  mounted () {
+    const decadeSections = document.querySelectorAll('.decades')
+    const decadeHeaders = document.querySelectorAll('.decade-title')
+
+    window.onscroll = () => {
+      const doc = document.documentElement
+      const scrollOffset = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0)
+      for (let index = 0; index < decadeSections.length; index++) {
+        const element = decadeSections[index];
+        const elementHeader = decadeHeaders[index]
+        const nextElement = index + 1 ? decadeHeaders[index + 1] : null
+
+        if(element.offsetTop <=  scrollOffset) {
+          element.style.paddingTop = elementHeader.clientHeight + 'px'
+
+          elementHeader.classList.add("heading-fixed")
+
+          if(nextElement && scrollOffset + elementHeader.clientHeight >= nextElement.offsetTop) {
+            elementHeader.style.position = "absolute"
+            elementHeader.style.top = nextElement.offsetTop - elementHeader.clientHeight + "px"
+          } else {
+            elementHeader.style.position = ""
+            elementHeader.style.top = ""
+          }
+
+        } else {
+          element.style.paddingTop = ''
+
+          elementHeader.classList.remove("heading-fixed")
+
+        }
+      }
+    }
+  },
 }
 </script>
 
@@ -58,19 +96,65 @@ export default {
 .date-list-container {
   &.decades {
     h2 {
-      display: inline;
+      display: inline-block;
       position: relative;
       font-size: 48px;
+      overflow: hidden;
+      width: 100%;
 
-      &::before {
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 24px;
-        background-color: #D9F4FE;
-        border-radius: 50px;
-        z-index: -1;
+      .inner {
+        margin: auto;
+        width: 95%;
+        max-width: 1200px;
+      }
+
+      &.heading-fixed {
+        position: fixed;
+        top: 0;
+        z-index: 10;
+      }
+
+      &.decade span {
+        animation: headingsAnimation .5s;
+      }
+
+      span {
+        position: relative;
         bottom: 0;
+        opacity: 1;
+        &::before {
+          content: '';
+          position: absolute;
+          width: 100%;
+          height: 24px;
+          background-color: #D9F4FE;
+          border-radius: 50px;
+          z-index: -1;
+          bottom: 0;
+          transform-origin: left;
+        }
+      }
+      &.decade::before {
+        animation: bgrAnimation .5s;
+      }
+
+      @keyframes headingsAnimation {
+        0% {
+          bottom: -10px;
+          opacity: 0;
+        }
+        100% {
+          bottom: 0;
+          opacity: 1;
+        }
+      }
+      @keyframes bgrAnimation {
+        0% {
+          transform: scaleX(0);
+        }
+        100% {
+          transform: scaleX(1);
+        }
       }
     }
   }
@@ -102,9 +186,6 @@ export default {
   width: 95%;
   margin: auto;
 
-  .date-list-container.no-decades > div {
-
-  }
   .date-list-container.decades section {
     @media (min-width: 720px) {
       display: grid;
